9eec9bf7124f659d29f992492a6ad9ab82dde57d
MDMahidul <mmbmahidul007@gmail.com>
2023-07-22 20:39:33 +0600

    update json data and run express js successfully

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000; //use this port to run the app
+const cors = require('cors')
+const data = require('./data/chef.json');
+app.use(cors());
+
+// Route handler for GET request to '/'
+app.get("/", (req, res) => {
+  res.send("Hello, chefs!");
+});
+
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
